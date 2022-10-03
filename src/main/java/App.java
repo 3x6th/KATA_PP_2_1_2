5e3a28d021f29b751d34c06a0738926d4a354232
smashbuster <smashbaster@gmail.com>
2022-10-03 23:17:59 +0700

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getName());
+        System.out.println(catBean2 == catBean);
     }
 }
